897c412ca140f02ef2d37d4f8a2e309423d2f942
Daniel Kiss <kissd621@gmail.com>
2021-06-23 17:51:45 +0200

    fix: add OmitType for proper typing

--- src/modules/reservation/services/create-reservation/dto/create-recurring-reservation.args.ts
+++ src/modules/reservation/services/create-reservation/dto/create-recurring-reservation.args.ts
@@ -1,9 +1,9 @@
-import { ArgsType, IntersectionType, PickType } from '@nestjs/graphql'
+import { ArgsType, IntersectionType, OmitType, PickType } from '@nestjs/graphql'
 import { IsRecurringTimeAvailableArgs } from '@reservation/services/times-availability/dto/is-recurring-time-available.args'
 import { CreateReservationArgs } from './create-reservation.args'
 
 @ArgsType()
 export class CreateRecurringReservationArgs extends IntersectionType(
-  IsRecurringTimeAvailableArgs,
+  OmitType(IsRecurringTimeAvailableArgs, [] as const),
   PickType(CreateReservationArgs, ['name']),
 ) {}

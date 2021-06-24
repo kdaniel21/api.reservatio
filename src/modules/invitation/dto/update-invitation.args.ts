ce80d679f4f8a6eb12cc3b943412ab6bc52f7c9f
Daniel Kiss <kissd621@gmail.com>
2021-06-24 08:20:22 +0200

    fix: make all properties optional except for ID

--- src/modules/invitation/dto/update-invitation.args.ts
+++ src/modules/invitation/dto/update-invitation.args.ts
@@ -1,5 +1,9 @@
-import { ArgsType, OmitType } from '@nestjs/graphql'
+import { ArgsType, IntersectionType, OmitType, PartialType, PickType } from '@nestjs/graphql'
 import { InvitationType } from './invitation.type'
 
 @ArgsType()
-export class UpdateInvitationArgs extends OmitType(InvitationType, ['inviter', 'inviterId', 'createdAt'], ArgsType) {}
+export class UpdateInvitationArgs extends IntersectionType(
+  PickType(InvitationType, ['id'] as const),
+  PartialType(OmitType(InvitationType, ['inviter', 'inviterId', 'createdAt', 'id'] as const)),
+  ArgsType,
+) {}
